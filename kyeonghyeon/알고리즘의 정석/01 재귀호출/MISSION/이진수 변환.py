bb8ed5e6791f9bc8723130423f862a8028253a73
LOTTECHILSUNG\kyeonghyeon.so <kyeonghyeon.so@lottechilsung.co.kr>
2021-12-06 18:27:36 +0900

    md 파일 디렉터리 변경, mission 이진수 변환 풀이

---
+++ kyeonghyeon/알고리즘의 정석/01 재귀호출/MISSION/이진수 변환.py
@@ -0,0 +1,32 @@
+import sys
+sys.setrecursionlimit(100000)
+
+def convertBinary(n) :
+    '''
+    10진수 n을 2진수로 변환하여 반환합니다.
+
+    *주의* : 변환된 2진수는 문자열이어야 합니다.
+
+    예를 들어, 19가 입력될 경우 문자열 "10011"이 반환되어야 합니다.
+    '''
+    quot = n // 2
+    remainder = n % 2
+
+    # base condition
+    if quot == 1:
+        return str(quot) + str(remainder)
+
+    # recursive
+    return convertBinary(quot) + str(remainder)
+
+def main():
+    '''
+    이 부분은 수정하지 마세요.
+    '''
+
+    n = int(input())
+
+    print(convertBinary(n))
+
+if __name__ == "__main__":
+    main()
